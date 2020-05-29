853eab88400f6345cdaca6971622e4b0096ff1a6
Deepak Kapoor <nk93555@gmail.com>
2020-05-29 13:32:13 +0530

    Updates

---
+++ precache-manifest.a11d37d0e17614a3f4b7b3743a77577c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2521a3d5e38bff297f894fbba1cb272c",
+    "url": "/PlantTalk/index.html"
+  },
+  {
+    "revision": "99bfb217392a9303c606",
+    "url": "/PlantTalk/static/css/2.08112f1f.chunk.css"
+  },
+  {
+    "revision": "f6aa3ce920536044e07d",
+    "url": "/PlantTalk/static/css/main.e048cf3c.chunk.css"
+  },
+  {
+    "revision": "99bfb217392a9303c606",
+    "url": "/PlantTalk/static/js/2.6dcbe9a6.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/PlantTalk/static/js/2.6dcbe9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6aa3ce920536044e07d",
+    "url": "/PlantTalk/static/js/main.f5ae7fa8.chunk.js"
+  },
+  {
+    "revision": "00139921883042d47d40",
+    "url": "/PlantTalk/static/js/runtime-main.0aec62b3.js"
+  },
+  {
+    "revision": "714ec8b37c8ff9ea4243d96612446e8d",
+    "url": "/PlantTalk/static/media/1.714ec8b3.jpg"
+  },
+  {
+    "revision": "904b534ffdd47701657c9802e67ac212",
+    "url": "/PlantTalk/static/media/2.904b534f.jpg"
+  },
+  {
+    "revision": "7509951cd68fa9f6e8772dc9f1b4ad64",
+    "url": "/PlantTalk/static/media/23.7509951c.jpg"
+  },
+  {
+    "revision": "ddc0992c82f4ccd1d5616aeebb997963",
+    "url": "/PlantTalk/static/media/24.ddc0992c.jpg"
+  },
+  {
+    "revision": "9d454ee179d3d26f48c8618d88d74b4d",
+    "url": "/PlantTalk/static/media/3.9d454ee1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PlantTalk/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PlantTalk/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PlantTalk/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PlantTalk/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PlantTalk/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/PlantTalk/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/PlantTalk/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/PlantTalk/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/PlantTalk/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/PlantTalk/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
